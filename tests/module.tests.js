85a24f775abc3299ca3370ed468a04efa2686205
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2021-10-01 20:47:22 -0300

    Add tests for the package dependencies
    These tests make sure that the most important dependencies are properly
    defined, preventing issues like the one raised in issue #60 from
    occurring again.

--- tests/module.tests.js
+++ tests/module.tests.js
@@ -1,13 +1,23 @@
 var expect = require('chai').expect;
 var rest = require('../src');
 var Client = require('../src/Client');
+var package = require('../package.json');
 
 module.exports = {
-  'REST-ORM module': {
-    'should expose the Client':
+  'rest-facade package': {
+    'should export the Client class':
       function () {
         expect(rest.Client).to.exist;
         expect(rest.Client).to.equal(Client);
+      },
+    'should have superagent as a dependency':
+      function () {
+        expect(package.dependencies).to.have.property('superagent');
+      },
+    'should have superagent-proxy as a peer dependency':
+      function () {
+        expect(package.dependencies).not.to.have.property('superagent-proxy');  
+        expect(package.peerDependencies).to.have.property('superagent-proxy');
       }
   }
 }
